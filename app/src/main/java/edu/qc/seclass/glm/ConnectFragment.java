a1a0b1f35fab9a62c5a9f0c705fa77d478096a55
Amit Charran <amit.charran@gmail.com>
2019-10-19 17:50:47 -0400

    For Jefferson to do Button on MapActivity

--- app/src/main/java/edu/qc/seclass/glm/ConnectFragment.java
+++ app/src/main/java/edu/qc/seclass/glm/ConnectFragment.java
@@ -1,11 +1,19 @@
 package edu.qc.seclass.glm;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,9 +35,24 @@ public class ConnectFragment extends Fragment {
      * @param savedInstanceState
      * @return
      */
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_connect,container,false);
+
+
+        View view = inflater.inflate(R.layout.fragment_connect,container,false);
+
+        
+        return view;
     }
+
+
+
+
+
+
+
+
 }
